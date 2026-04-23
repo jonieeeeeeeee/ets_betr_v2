f783f6a810ab2815f9925ba36ef12dc77cb34c5f
Matevo <73072244+Matevoo@users.noreply.github.com>
2026-04-23 12:48:39 +0200

    update

--- svelte.config.js
+++ svelte.config.js
@@ -4,15 +4,11 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
   kit: {
     adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: '404.html',
-      precompress: false,
-      strict: false
-    }),
-    prerender: {
-      handleHttpError: 'warn'
-    }
+      pages:       'build',
+      assets:      'build',
+      fallback:    'index.html',
+      precompress: false
+    })
   }
 };
 
